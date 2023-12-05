390583d34e5b952a3f4c7f6f4079887e3917c24e
hagd <hagd0520@gmail.com>
2023-12-05 21:30:14 +0900

    1-02 layout.html

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,16 +1,33 @@
 package com.ll.medium.domain.member.member.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
+
+    @Getter
+    @RequiredArgsConstructor
+    public static class JoinForm {
+        private final String username;
+        private final String password;
+        private final String passwordConfirm;
+    }
+
     @GetMapping("/join")
     public String showJoin() {
         return "domain/member/member/join";
     }
+
+    @PostMapping("/join")
+    public String doJoin(JoinForm joinForm) {
+        return null;
+    }
+
 }
