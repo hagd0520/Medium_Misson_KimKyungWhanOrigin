9fd105c76a4dafe7f4015dee37ceba91f0159ebf
hagd <hagd0520@gmail.com>
2023-12-07 22:22:31 +0900

    0-2 NotProd

--- src/main/java/com/ll/medium/global/initData/NotProd.java
+++ src/main/java/com/ll/medium/global/initData/NotProd.java
@@ -2,6 +2,7 @@ package com.ll.medium.global.initData;
 
 import com.ll.medium.domain.article.article.service.ArticleService;
 import com.ll.medium.domain.base.system.SystemService;
+import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.service.MemberService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,10 @@ public class NotProd {
     ) {
         return args -> {
             if (systemService.isSampleDataCreated()) return;
+
+            Member memberAdmin = memberService.join("admin", "1234", "1234").getData();
+            Member memberUser1 = memberService.join("user1", "1234", "1234").getData();
+            Member memberUser2 = memberService.join("user2", "1234", "1234").getData();
         };
     }
 }
