baf97919d71de8675a9d4d0dd463e402dfe61346
hagd <hagd0520@gmail.com>
2023-12-11 21:17:43 +0900

    T-03 MemberControllerTest t4, t5, t6

--- src/test/java/com/ll/medium/domain/member/member/controller/MemberControllerTest.java
+++ src/test/java/com/ll/medium/domain/member/member/controller/MemberControllerTest.java
@@ -15,7 +15,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -119,5 +122,54 @@ public class MemberControllerTest {
                 .andExpect(status().is4xxClientError())
                 .andExpect(handler().handlerType(MemberController.class))
                 .andExpect(handler().methodName("join"));
+    }
+
+    @Test
+    @DisplayName("로그인 페이지를 보여준다.")
+    void t4() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/member/login"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(view().name("member/member/login"))
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showLogin"))
+                .andExpect(content().string(containsString("""
+                        로그인
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="text" name="username"
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="password" name="password"
+                        """.stripIndent().trim())));
+    }
+
+    @Test
+    @DisplayName("잘못된 로그인 정보")
+    void t5() throws Exception {
+        mvc.perform(
+                        formLogin("/member/login")
+                                .user("username", "user1")
+                                .password("password", "12345")
+                )
+                .andDo(print())
+                .andExpect(unauthenticated());
+    }
+
+    @Test
+    @DisplayName("로그인 처리")
+    void t6() throws Exception {
+        mvc.perform(
+                        formLogin("/member/login")
+                                .user("username", "user1")
+                                .password("password", "1234")
+                )
+                .andDo(print())
+                .andExpect(authenticated());
     }
 }
