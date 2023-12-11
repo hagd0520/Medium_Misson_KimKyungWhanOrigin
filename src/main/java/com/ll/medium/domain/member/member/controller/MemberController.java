9d75b121a135b341a3cb67c53026259df485f2a8
hagd <hagd0520@gmail.com>
2023-12-11 20:46:23 +0900

    2-04 login 기능 구현

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import com.ll.medium.global.rq.Rq;
 import com.ll.medium.global.rsData.RsData;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,11 +21,13 @@ public class MemberController {
     private final MemberService memberService;
     private final Rq rq;
 
+    @PreAuthorize("isAnonymous()")
     @GetMapping("/join")
     public String showJoin() {
         return "member/member/join";
     }
 
+    @PreAuthorize("isAnonymous()")
     @PostMapping("/join")
     public String join(@Valid MemberJoinForm joinForm, BindingResult bindingResult) {
 
@@ -37,6 +40,7 @@ public class MemberController {
         return rq.redirectOrBack("/member/login", joinRs);
     }
 
+    @PreAuthorize("isAnonymous()")
     @GetMapping("/login")
     public String showLogin() {
 
