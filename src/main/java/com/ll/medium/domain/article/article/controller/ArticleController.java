5d93a986df6db35db2499a7b501b8c4308c813df
hagd <hagd0520@gmail.com>
2023-12-12 15:32:29 +0900

    4-03 게시글 목록 초안 2

--- src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
@@ -1,10 +1,15 @@
 package com.ll.medium.domain.article.article.controller;
 
+import com.ll.medium.domain.article.article.entity.Article;
 import com.ll.medium.domain.article.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/article")
@@ -13,9 +18,15 @@ public class ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("/list")
-    public String showList() {
+    public String showList(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<Article> paging = articleService.getList(page);
+        model.addAttribute("paging", paging);
         return "article/article/list";
     }
 
-
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/article/write")
+    public String showWrite() {
+        return "article/article/write";
+    }
 }
--- src/main/java/com/ll/medium/domain/article/article/service/ArticleService.java
+++ src/main/java/com/ll/medium/domain/article/article/service/ArticleService.java
@@ -1,11 +1,27 @@
 package com.ll.medium.domain.article.article.service;
 
+import com.ll.medium.domain.article.article.entity.Article;
 import com.ll.medium.domain.article.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
+
+    public Page<Article> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return articleRepository.findAll(pageable);
+    }
+
 }
